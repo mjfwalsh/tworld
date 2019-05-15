cd6217af854f4f3c18e10ea261404f266bc82173
Michael J. Walsh <mjfwalsh@gmail.com>
2019-05-15 16:19:59 +0100

    Misc changes to make scripts

--- make.sh
+++ make.sh
@@ -3,14 +3,15 @@ PATH=$PATH:/usr/local/opt/qt/bin
 C_BASE="cc -Wall -pedantic -O2 -I. -DNDEBUG -Dstricmp=strcasecmp -DTWPLUSPLUS -std=gnu11 -Werror"
 CPP_BASE="c++ -Wall -pedantic -O2 -I. -DNDEBUG -Dstricmp=strcasecmp -DTWPLUSPLUS -std=gnu++11 -Werror"
 SDL_OPTS='-I/usr/local/include/SDL -D_GNU_SOURCE=1 -D_THREAD_SAFE'
-QT_OPTS='-I/usr/local/Cellar/qt/5.12.3/include -I/usr/local/Cellar/qt/5.12.3/include/QtCore -I/usr/local/Cellar/qt/5.12.3/include/QtGui -I/usr/local/Cellar/qt/5.12.3/include/QtXml -I/usr/local/Cellar/qt/5.12.3/include/QtWidgets'
+QT_OPTS='-I/usr/local/opt/qt/include -I/usr/local/opt/qt/include/QtCore -I/usr/local/opt/qt/include/QtGui -I/usr/local/opt/qt/include/QtXml -I/usr/local/opt/qt/include/QtWidgets'
+PRINT_DIR=''
 
 run () {
 	echo "$@"
 	"$@"
 }
 
-make_file2 () {
+make_file () {
 	if [ -e $3 ]
 	then 
 		echo "Nothing to do for $4"
@@ -26,70 +27,94 @@ make_file2 () {
 	fi
 }
 
-make_file () {
-	make_file2 "Compiling $3..." "$@"
+compile_file () {
+	make_file "Compiling $PRINT_DIR$3..." "$1 -c -o" "$2" "$3"
+}
+
+compile () {
+	compile_file "$C_BASE" tworld.o tworld.c
+	compile_file "$C_BASE" series.o series.c
+	compile_file "$C_BASE" play.o play.c
+	compile_file "$C_BASE" encoding.o encoding.c
+	compile_file "$C_BASE" solution.o solution.c
+	compile_file "$C_BASE" res.o res.c
+	compile_file "$C_BASE" lxlogic.o lxlogic.c
+	compile_file "$C_BASE" mslogic.o mslogic.c
+	compile_file "$C_BASE" unslist.o unslist.c
+	compile_file "$CPP_BASE" messages.o messages.cpp
+	echo Generating comptime.h...
+	echo \#define COMPILE_TIME \"`date '+%Y %b %e %T %Z'`\" \> comptime.h
+	echo \#define COMPILE_TIME \"`date '+%Y %b %e %T %Z'`\" > comptime.h
+	compile_file "$C_BASE" help.o help.c
+	compile_file "$CPP_BASE" score.o score.cpp
+	compile_file "$C_BASE" random.o random.c
+	compile_file "$C_BASE" cmdline.o cmdline.c
+	compile_file "$CPP_BASE" settings.o settings.cpp
+	compile_file "$C_BASE" fileio.o fileio.c
+	compile_file "$C_BASE" err.o err.c
+
+	cd oshw-qt
+	PRINT_DIR='generic/'
+	compile_file "$C_BASE $SDL_OPTS" generic.o ../generic/generic.c
+	compile_file "$C_BASE $SDL_OPTS" tile.o ../generic/tile.c
+	compile_file "$C_BASE $SDL_OPTS" timer.o ../generic/timer.c
+	compile_file "$CPP_BASE $QT_OPTS" _in.o ../generic/_in.cpp
+	PRINT_DIR='oshw-qt/'
+	compile_file "$C_BASE $SDL_OPTS" _sdlsfx.o _sdlsfx.c
+	compile_file "$CPP_BASE $QT_OPTS" oshwbind.o oshwbind.cpp
+	compile_file "$CPP_BASE $QT_OPTS" CCMetaData.o CCMetaData.cpp
+	compile_file "$CPP_BASE $QT_OPTS" TWDisplayWidget.o TWDisplayWidget.cpp
+	compile_file "$CPP_BASE $QT_OPTS" TWProgressBar.o TWProgressBar.cpp
+	make_file "Compiling UI oshw-qt/TWMainWnd.ui..." "uic -o" ui_TWMainWnd.h TWMainWnd.ui
+	compile_file "$CPP_BASE $QT_OPTS" TWMainWnd.o TWMainWnd.cpp
+	make_file "MOC-ing oshw-qt/TWMainWnd.h..." "moc -o" moc_TWMainWnd.cpp TWMainWnd.h
+	compile_file "$CPP_BASE $QT_OPTS" moc_TWMainWnd.o moc_TWMainWnd.cpp
+	compile_file "$CPP_BASE $QT_OPTS" TWApp.o TWApp.cpp
+	cd ..
+	PRINT_DIR=''
+
+	echo Linking tworld2...
+	run c++ -o tworld2 tworld.o series.o play.o encoding.o solution.o res.o lxlogic.o mslogic.o unslist.o messages.o help.o score.o random.o cmdline.o settings.o fileio.o err.o \
+	oshw-qt/generic.o oshw-qt/tile.o oshw-qt/timer.o oshw-qt/_in.o oshw-qt/_sdlsfx.o oshw-qt/oshwbind.o oshw-qt/CCMetaData.o oshw-qt/TWDisplayWidget.o oshw-qt/TWProgressBar.o oshw-qt/TWMainWnd.o oshw-qt/moc_TWMainWnd.o oshw-qt/TWApp.o \
+	-L/usr/local/opt/qt/lib -F/usr/local/opt/qt/Frameworks -framework QtCore -framework QtGui -framework QtXml -framework QtWidgets -L/usr/local/lib -lSDLmain -lSDL -Wl,-framework,Cocoa
+	if [ $? -eq 0 ]
+	then
+		echo Done...
+	else
+		echo Failed...
+	fi
+}
+
+clean () {
+	rm -fR Tile\ World.app
+	rm -f cmdline.o comptime.h encoding.o err.o fileio.o help.o lxlogic.o messages.o mslogic.o oshw-qt/_in.o oshw-qt/_sdlsfx.o oshw-qt/CCMetaData.o oshw-qt/generic.o oshw-qt/moc_TWMainWnd.cpp oshw-qt/moc_TWMainWnd.o oshw-qt/oshwbind.o oshw-qt/tile.o oshw-qt/timer.o oshw-qt/TWApp.o oshw-qt/TWDisplayWidget.o oshw-qt/TWMainWnd.o oshw-qt/TWProgressBar.o oshw-qt/ui_TWMainWnd.h play.o random.o res.o score.o series.o settings.o solution.o tworld.o tworld2 unslist.o
+}
+
+mkapp () {
+	mkdir -p Tile\ World.app/Contents/MacOS
+	mkdir -p Tile\ World.app/Contents/Resources
+	cp Info.plist Tile\ World.app/Contents/
+	cp Tile\ World.icns Tile\ World.app/Contents/Resources/
+	cp tworld2 Tile\ World.app/Contents/MacOS/Tile\ World
+	
+	cp -R res Tile\ World.app/Contents/Resources/
+	cp -R sets Tile\ World.app/Contents/Resources/
+	cp -R data Tile\ World.app/Contents/Resources/
 }
 
 
-make_file "$C_BASE -c -o" tworld.o tworld.c
-make_file "$C_BASE -c -o" series.o series.c
-make_file "$C_BASE -c -o" play.o play.c
-make_file "$C_BASE -c -o" encoding.o encoding.c
-make_file "$C_BASE -c -o" solution.o solution.c
-make_file "$C_BASE -c -o" res.o res.c
-make_file "$C_BASE -c -o" lxlogic.o lxlogic.c
-make_file "$C_BASE -c -o" mslogic.o mslogic.c
-make_file "$C_BASE -c -o" unslist.o unslist.c
-make_file "$CPP_BASE -c -o" messages.o messages.cpp
-echo Generating comptime.h...
-echo \#define COMPILE_TIME \"`date '+%Y %b %e %T %Z'`\" > comptime.h
-make_file "$C_BASE -c -o" help.o help.c
-make_file "$CPP_BASE -c -o" score.o score.cpp
-make_file "$C_BASE -c -o" random.o random.c
-make_file "$C_BASE -c -o" cmdline.o cmdline.c
-make_file "$CPP_BASE -c -o" settings.o settings.cpp
-make_file "$C_BASE -c -o" fileio.o fileio.c
-make_file "$C_BASE -c -o" err.o err.c
 
-echo --- Building oshw-qt...
-
-cd oshw-qt
-make_file "$C_BASE $SDL_OPTS -c -o" generic.o ../generic/generic.c
-make_file "$C_BASE $SDL_OPTS -c -o" tile.o ../generic/tile.c
-make_file "$C_BASE $SDL_OPTS -c -o" timer.o ../generic/timer.c
-make_file "$CPP_BASE $QT_OPTS -c -o" _in.o ../generic/_in.cpp
-make_file "$C_BASE $SDL_OPTS -c -o" _sdlsfx.o _sdlsfx.c
-make_file "$CPP_BASE $QT_OPTS -c -o" oshwbind.o oshwbind.cpp
-make_file "$CPP_BASE $QT_OPTS -c -o" CCMetaData.o CCMetaData.cpp
-make_file "$CPP_BASE $QT_OPTS -c -o" TWDisplayWidget.o TWDisplayWidget.cpp
-make_file "$CPP_BASE $QT_OPTS -c -o" TWProgressBar.o TWProgressBar.cpp
-
-
-make_file2 "Compiling UI TWMainWnd.ui..." "uic -o" ui_TWMainWnd.h TWMainWnd.ui
-make_file "$CPP_BASE $QT_OPTS -c -o" TWMainWnd.o TWMainWnd.cpp
-make_file2 "MOC-ing TWMainWnd.h..." "moc -o" moc_TWMainWnd.cpp TWMainWnd.h
-make_file "$CPP_BASE $QT_OPTS -c -o" moc_TWMainWnd.o moc_TWMainWnd.cpp
-make_file "$CPP_BASE $QT_OPTS -c -o" TWApp.o TWApp.cpp
-echo ---
-cd ..
-mkdir -p Tile\ World.app/Contents/MacOS
-mkdir -p Tile\ World.app/Contents/Resources
-cp Info.plist Tile\ World.app/Contents/
-cp Tile\ World.icns Tile\ World.app/Contents/Resources/
-
-cd Tile\ World.app/Contents/Resources
-ln -s $OLDPWD/res .
-ln -s $OLDPWD/sets .
-ln -s $OLDPWD/data .
-cd $OLDPWD
-
-echo Linking tworld2...
-run c++ -o ./Tile\ World.app/Contents/MacOS/Tile\ World tworld.o series.o play.o encoding.o solution.o res.o lxlogic.o mslogic.o unslist.o messages.o help.o score.o random.o cmdline.o settings.o fileio.o err.o \
-oshw-qt/generic.o oshw-qt/tile.o oshw-qt/timer.o oshw-qt/_in.o oshw-qt/_sdlsfx.o oshw-qt/oshwbind.o oshw-qt/CCMetaData.o oshw-qt/TWDisplayWidget.o oshw-qt/TWProgressBar.o oshw-qt/TWMainWnd.o oshw-qt/moc_TWMainWnd.o oshw-qt/TWApp.o \
--L/usr/local/Cellar/qt/5.12.3/lib -F/usr/local/opt/qt/Frameworks -framework QtCore -framework QtGui -framework QtXml -framework QtWidgets -L/usr/local/lib -lSDLmain -lSDL -Wl,-framework,Cocoa
-if [ $? -eq 0 ]
-then
-	echo Done...
+if [ "$1" = "install" ]; then
+	compile
+	mkapp
+	mv Tile\ World.app /Applications/
+elif [ "$1" = "compile" ]; then
+	compile
+elif [ "$1" = "mkapp" ]; then
+	mkapp
+elif [ "$1" = "clean" ]; then
+	clean
 else
-	echo Failed...
+	compile
 fi
+
